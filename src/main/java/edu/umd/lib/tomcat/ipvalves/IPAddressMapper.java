e4848bbdf1fa07a0cc2b42425648c53acadf4691
Jeremy Matthew Gottwig <jgottwig@umd.edu>
2016-07-13 16:23:43 -0400

    LIBFCREPO-58. Enhancements; fixes -Classpath issue -Caching properties -Additional repairs
    https://issues.umd.edu/browse/LIBFCREPO-58

--- src/main/java/edu/umd/lib/tomcat/ipvalves/IPAddressMapper.java
+++ src/main/java/edu/umd/lib/tomcat/ipvalves/IPAddressMapper.java
@@ -5,20 +5,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
 
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.net.util.SubnetUtils;
+import org.apache.commons.validator.routines.InetAddressValidator;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.apache.tomcat.util.buf.MessageBytes;
-
-import com.google.common.net.InetAddresses;
 
 /**
  * This valve checks a user's IP address against a properties file containing
@@ -28,7 +30,7 @@ import com.google.common.net.InetAddresses;
  *
  * The properties file should follow the following format:
  *
- * propertyName1=propertyValue1 propertyName2=propertyValue2
+ * propertyName1=propertyValue1
  *
  * The valve expects the following configuration format and options:
  *
@@ -40,7 +42,7 @@ import com.google.common.net.InetAddresses;
  * @author jgottwig
  */
 
-public class IPAddressMapper extends ValveBase {
+public class IPAddressMapper extends ValveBase implements Lifecycle {
 
   protected static final String info = "edu.umd.lib.tomcat.ipvalves.IPAddressMapper/0.0.1";
 
@@ -48,6 +50,16 @@ public class IPAddressMapper extends ValveBase {
 
   private String mappingFile;
   private String headerName;
+
+  private Properties properties = new Properties();
+
+  @Override
+  protected void initInternal() throws LifecycleException {
+    super.initInternal();
+    if (checkProperties() || loadProperties()) {
+      log.warn("IPProperties: Not found");
+    }
+  }
 
   /**
    * Constructor
@@ -83,101 +95,57 @@ public class IPAddressMapper extends ValveBase {
     this.headerName = headerName;
   }
 
-  @Override
-  public void invoke(Request request, Response response) throws IOException, ServletException {
+  /**
+   * Check for valid IP
+   *
+   * @param ip
+   * @return boolean (valid IP)
+   */
+  protected boolean isValidIP(String ip) {
+    log.info("IPVali: init");
+    return InetAddressValidator.getInstance().isValidInet4Address(ip);
+  }
 
-    /**
-     * General TODOs
-     *
-     * @TODO Determine development environment (cargo, tomcat, etc?)
-     * @TODO Get complete list of IPs (later)
-     * @TODO Determine application structure
-     */
-
-    /**
-     * Attempt to load our properties file
-     *
-     * @TODO Test
-     */
-    Properties properties = new Properties();
-    InputStream input = null;
-
-    try {
-      input = new FileInputStream(this.mappingFile);
-      properties.load(input);
-    } catch (IOException e) {
-      e.printStackTrace();
-      log.error(e);
-      // No reason to go beyond this point.
-      getNext().invoke(request, response);
-    } finally {
-      if (input != null) {
-        try {
-          input.close();
-        } catch (IOException ex) {
-          ex.printStackTrace();
-        }
-      }
-    }
-
-    /**
-     * Check user headers for existing header. This is necessary to prevent
-     * spoofing. If the header already exists, strip and reevaluate.
-     *
-     * @TODO Strip if exists *done*
-     * @TODO Log alert if exists *done*
-     * @TODO Test
-     */
-    MessageBytes storedHeader = request.getCoyoteRequest().getMimeHeaders().getValue(this.headerName);
-    if (storedHeader != null) {
-      log.warn("Header: " + storedHeader + " found before IP mapper eval!");
-      request.getCoyoteRequest().getMimeHeaders().removeHeader(this.headerName);
-    }
-
-    /**
-     * Get user IP. For now, we are assuming only IPV4.
-     *
-     * @TODO Get user IP
-     * @TODO Test
-     * @note Is proxy support needed?
-     */
-    String rawIP = request.getHeader("X-FORWARDED-FOR");
+  /**
+   * Get user IP from the request
+   *
+   * @param request
+   * @return String (userIP)
+   */
+  protected String retrieveIP(Request request) {
     String userIP = null;
+    String rawIP = request.getHeader("X-FORWARDED-FOR");
     if (rawIP == null) {
       userIP = request.getRemoteAddr();
+      log.info("IPNorm: " + userIP);
     } else {
       /**
        * It's possible we might get a comma-separated list of IPs, in which
        * case, we should split prior to evaluation. Real IP should always come
        * first. This doesn't look pretty though.
        */
+      log.info("IPRaw: " + rawIP);
       String[] userIPs = rawIP.split(",");
       if (userIPs[0] != null) {
         userIP = userIPs[0].trim();
+        log.info("IPProx: " + userIP);
       }
     }
+    log.info("IPReturning: " + userIP);
+    return userIP;
+  }
 
-    /**
-     * Validate the IP address before continuing.
-     *
-     * @TODO Test
-     */
-    if (!InetAddresses.isInetAddress(userIP)) {
-      log.warn("IP: " + userIP + " detected as invalid!");
-      getNext().invoke(request, response);
-    }
-
-    /**
-     * Compare user IP to properties blocks.
-     *
-     * @TODO Find a good IP comparison library *done*
-     * @TODO Write comparison logic. It must support CIDR *done*
-     * @TODO Loop through properties data *done*
-     * @TODO Test
-     */
+  /**
+   * Examine IP and compare against subnets from properties
+   *
+   * @param ip
+   * @return List (approval strings)
+   */
+  protected List<String> getApprovals(String ip) {
     Enumeration<?> propertyNames = properties.propertyNames();
 
-    ArrayList<String> approvals = new ArrayList<String>();
+    List<String> approvals = new ArrayList<String>();
+    log.info("IPBefore");
     SubnetUtils utils; // Our comparison library
 
     /**
@@ -189,27 +157,123 @@ public class IPAddressMapper extends ValveBase {
       String property = properties.getProperty(key);
       String[] subnets = property.split(",");
       for (String subnet : subnets) {
+        log.info("IPip: " + subnet);
         utils = new SubnetUtils(subnet);
-        if (utils.getInfo().isInRange(userIP)) {
+        log.info("IPAfter: " + utils);
+        if (utils.getInfo().isInRange(ip)) {
+          log.info("IPMatch");
           approvals.add(key);
+        } else {
+          log.info("IPNoMatch");
         }
       }
     }
+    return approvals;
+  }
+
+  protected boolean checkProperties() {
+    Enumeration<?> propertyNames = properties.elements();
+    return propertyNames.hasMoreElements();
+  }
+
+  /**
+   * Load the properties file
+   *
+   * @return boolean (success)
+   */
+  protected boolean loadProperties() {
+    boolean success = false;
+    log.info("IPProps: NoProps");
+    InputStream input = null;
+    try {
+      input = new FileInputStream(mappingFile);
+      properties.load(input);
+      log.info("IPAddress: " + properties.getProperty("campus"));
+      success = true;
+    } catch (IOException e) {
+      e.printStackTrace();
+      log.error(e);
+    } finally {
+      if (input != null) {
+        try {
+          input.close();
+        } catch (IOException ex) {
+          ex.printStackTrace();
+        }
+      }
+    }
+    return success;
+  }
+
+  @Override
+  public void invoke(Request request, Response response) throws IOException, ServletException {
+
+    log.info(request);
+    log.info("IPMapping File: " + mappingFile);
+    log.info("IPHeader Name: " + headerName);
 
     /**
-     * Inject the header with value if the user's IP meets the above criteria.
+     * Attempt to load our properties file
      *
-     * @TODO Determine what happens if the criteria doesn't match (nothing?)
-     * @TODO Write header injection
+     * @TODO Move this into some sort of cache method?
+     * @TODO Look at lifecycle stuff
      * @TODO Test
      */
-    String finalHeaders = null;
-    if (!approvals.isEmpty()) {
-      finalHeaders = StringUtils.join(approvals, ",");
-      MessageBytes newHeader = request.getCoyoteRequest().getMimeHeaders().addValue(this.headerName);
-      newHeader.setString(finalHeaders);
-    }
+    if (checkProperties()) {
 
-    getNext().invoke(request, response);
+      /**
+       * Check user headers for existing header. This is necessary to prevent
+       * spoofing. If the header already exists, strip and reevaluate.
+       *
+       * @TODO Strip if exists *done*
+       * @TODO Log alert if exists *done*
+       * @TODO Test
+       */
+      MessageBytes storedHeader = request.getCoyoteRequest().getMimeHeaders().getValue(headerName);
+      if (storedHeader != null) {
+        log.warn("Header: " + storedHeader + " found before IP mapper eval!");
+        request.getCoyoteRequest().getMimeHeaders().removeHeader(headerName);
+      } else {
+        log.info("IPHeader: Not found");
+      }
+
+      /**
+       * Get user IP. For now, we are assuming only IPV4.
+       *
+       * @TODO Get user IP
+       * @TODO Test
+       * @note Is proxy support needed?
+       */
+      String userIP = retrieveIP(request);
+
+      log.info("IPBreak");
+
+      if (isValidIP(userIP)) {
+        log.info("passed");
+        /**
+         * Compare user IP to properties IPs
+         */
+        List<String> approvals = getApprovals(userIP);
+
+        /**
+         * Inject the header with value if the user's IP meets the above
+         * criteria.
+         *
+         * @TODO Determine what happens if the criteria doesn't match (nothing?)
+         * @TODO Write header injection
+         * @TODO Test
+         */
+        String finalHeaders = null;
+        if (!approvals.isEmpty()) {
+          finalHeaders = StringUtils.join(approvals, ",");
+          log.info("IPNew: " + finalHeaders);
+          request.getCoyoteRequest().getMimeHeaders().setValue(headerName).setString(finalHeaders);
+        }
+      } // @end validIP
+    } // @end loadProperties
+    log.info("IPHeader: " + request.getHeader(headerName));
+    log.info("IPResponse: " + response.getHeader(headerName));
+    log.info("IPTest: " + response.getHeader("Content-Type"));
+    getNext().invoke(request, response); // Junit for testing
   }
 }
